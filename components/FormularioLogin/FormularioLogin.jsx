a4a9ffb78969344a72757c3989b9cf94314f4d4a
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-02-02 22:15:10 -0300

    novo alinhamento da Home

--- components/FormularioLogin/FormularioLogin.jsx
+++ components/FormularioLogin/FormularioLogin.jsx
@@ -8,58 +8,56 @@ import {
   Button,
   Image,
   Grid,
+ 
 } from "@chakra-ui/react";
 
 function FormularioLogin() {
+
+
   return (
-    <Flex justifySelf="center" >
-      <Container
-        margin="auto"
+    <Flex  position="static" display="flex" justify="center" align="center" flexWrap="wrap" bgColor="black"
+    >
+      <Container 
+        p="14"
+        margin="32"
+        position="center"
         display="flex"
-        flex=""
+        border="2px"
+        borderRadius="base"
+        borderStyle="solid"
+        maxW="sm"
+        minW="sm"
         centerContent="true"
         justifySelf="center"
+        alignContent="center"
+        bgColor="white"
       >
-        <Container
-          p="14"
-          margin="24"
-          position="absolute"
-          display="flex"
-          border="2px"
-          borderRadius="base"
-          borderStyle="solid"
-          maxW="sm"
-          minW="sm"
-          centerContent="true"
-          justifySelf="center"
-        >
-          <Image
-            borderRadius="full"
-            boxShadow="md"
-            boxSize="50px"
-            src="https://www.protectivelife.com.bd/images/icons/Signin.png"
-            fallbackSrc="https://via.placeholder.com/150"
-            alt="login vector"
-          />
-          Bem-vindo ao EstoqueFacil!
-          <Stack h="auto" w="100%" justify="center" align="center" spacing={3}>
-            <InputGroup>
-              <Input
-                required
-                box-sizing="content-box"
-                type="user"
-                placeholder="username"
-              />
-            </InputGroup>
+        <Image
+          borderRadius="full"
+          boxShadow="md"
+          boxSize="50px"
+          src="https://www.protectivelife.com.bd/images/icons/Signin.png"
+          fallbackSrc="https://via.placeholder.com/150"
+          alt="login vector"
+        />
+        Bem-vindo ao EstoqueFacil!
+        <Stack h="auto" w="100%" justify="center" align="center" spacing={3}>
+          <InputGroup>
+            <Input
+              required
+              box-sizing="content-box"
+              type="user"
+              placeholder="username"
+            />
+          </InputGroup>
 
-            <InputGroup>
-              <Input required type="senha" placeholder="senha" />
-            </InputGroup>
-            <Button onCLick="">Login</Button>
-          </Stack>
-        </Container>
+          <InputGroup>
+            <Input required type="senha" placeholder="senha" />
+          </InputGroup>
+          <Button onCLick="">Login</Button>
+        </Stack>
       </Container>
-    </Flex>
+      </Flex>
   );
 }
 
